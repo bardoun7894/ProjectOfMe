18162679510d900f951c842fdd8e6bced0e473ec
bardouni mohamed <mbardouni44@gmail.com>
2017-12-04 17:11:46 +0000

    part 4: ViewType for RedditNewsItem

--- app/src/main/java/com/example/bardouni/rxproject/RedditNewsItem.kt
+++ app/src/main/java/com/example/bardouni/rxproject/RedditNewsItem.kt
@@ -1,5 +1,8 @@
 package com.example.bardouni.rxproject
 
+import com.example.bardouni.rxproject.commons.adapter.AdapterConstants
+import com.example.bardouni.rxproject.commons.adapter.ViewType
+
 /**
  * Created by bardouni on 12/4/2017.
  */
@@ -11,4 +14,6 @@ data class RedditNewsItem(
             val created: Long,
        val thumbnail: String,
             val url: String
-    )
+    ) : ViewType {
+       override fun getViewType() = AdapterConstants.NEWS
+    }
